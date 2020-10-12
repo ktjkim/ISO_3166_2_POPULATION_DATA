5eaf6688fb350cdfd7fec8ea4ad3a308ac479368
ktjkim <37836749+ktjkim@users.noreply.github.com>
2020-10-12 01:14:16 -0400

    Albania, Åland Islands, Andorra

--- subdivision_populations.py
+++ subdivision_populations.py
@@ -43,3 +43,51 @@ population['AF-PKA'] = 762108
 population['AF-PAN'] = 167000
 population['AF-DAY'] = 507610
 
+# Angola
+# official bureau of statistics page cannot be accessed with secure connection (Oct 12th, 2020) 
+# population['AO-CUS'] = None
+
+# Anguilla
+# 'AI-X00', 'AI-X01~', 'AI-X02~', 'AI-X03~', 'AI-X04~', 'AI-X05~', 'AI-X06~', 'AI-X07~', 'AI-X08~', 'AI-X09~', 'AI-X10~', 'AI-X11~', 'AI-X12~', 'AI-X13~', 'AI-X14~'
+
+# Albania (Jan 1st, 2020) 
+# http://www.instat.gov.al/media/6850/population-on-1-january-2020___.pdf
+population['AL-02'] = 290,697
+population['AL-04'] = 289,889
+population['AL-10'] = 200,007
+population['AL-07'] = 75,428
+population['AL-VL'] = 188,922
+population['AL-06'] = 204,831
+population['AL-BR'] = 122,003
+population['AL-EL'] = 270,074
+population['AL-GJ'] = 59,381
+population['AL-DI'] = 115,857
+population['AL-LE'] = 122,700
+population['AL-DR'] = 906,166
+# MUST EDIT: THE KEY 'AL-DR' FOR THE FINAL LINE IS WRONG. THIS MUST CORRESPOND TO Tiranë, OR 'AL-11'. 
+# MUST EDIT: THE ORIGINAL DATA FILE MAINTAINS A MIX OF LETTER CODES AND NUMERIC CODES. THIS MUST BE FIXED. 
+
+# Åland Islands (2019)
+# https://www.asub.ax/en/statistics/population/size-and-structure-population
+population['AX-X02~'] = 5233
+population['AX-X04~'] = 952
+population['AX-X06~'] = 531
+population['AX-X09~'] = 11679
+population['AX-X10~'] = 2053
+population['AX-X11~'] = 366
+population['AX-X12~'] = 447
+population['AX-X13~'] = 88
+population['AX-X14~'] = 314
+population['AX-X15~'] = 445
+population['AX-X16~'] = 232
+
+# Andorra (2019)
+# https://www.estadistica.ad/serveiestudis/web/banc_dades4.asp?lang=4&codi_tema=2&codi_divisio=2162&codi_subtemes=8
+population['AD-07'] = 22440
+population['AD-02'] = 4325
+population['AD-03'] = 11688
+population['AD-08'] = 14599
+population['AD-04'] = 10174
+population['AD-05'] = 4942
+population['AD-06'] = 9375
+
