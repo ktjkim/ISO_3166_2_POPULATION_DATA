b8bb9a4399d1afce73915dedee3e4bdcfe1478b1
ktjkim <37836749+ktjkim@users.noreply.github.com>
2020-10-12 00:46:36 -0400

    Aruba and Afghanistan

--- subdivision_populations.py
+++ subdivision_populations.py
@@ -1,3 +1,45 @@
 from subdivision_names import names
 
 population = dict.fromkeys(names, None)
+
+# Aruba 
+# https://cbs.aw/wp/index.php/2020/06/15/quarterly-demographic-bulletin-2019-2/
+population['AW-X01'] = 112269 
+
+# Afghanistan
+# https://www.nsia.gov.af:8080/wp-content/uploads/2020/05/Afghanistan-Statistical-Yearbook-2019-1st-Version.pdf
+population['AF-BDG'] = 540009
+population['AF-HER'] = 2095117
+population['AF-BAM'] = 486928
+population['AF-BAL'] = 1475649
+population['AF-FYB'] = 1089228
+population['AF-JOW'] = 590866
+population['AF-GHO'] = 751254
+population['AF-SAR'] = 609986
+population['AF-FRA'] = 553058
+population['AF-HEL'] = 1420682
+population['AF-NIM'] = 180200
+population['AF-URU'] = 428466
+population['AF-KAN'] = 1368036
+population['AF-ZAB'] = 377648
+population['AF-GHA'] = 1338597
+population['AF-KHO'] = 625473
+population['AF-PIA'] = 601230
+population['AF-BDS'] = 1035658
+population['AF-NUR'] = 160993
+population['AF-KNR'] = 490690
+population['AF-KDZ'] = 1113676
+population['AF-NAN'] = 1668481
+population['AF-TAK'] = 1073319
+population['AF-BGL'] = 995814
+population['AF-KAB'] = 5029850
+population['AF-KAP'] = 479875
+population['AF-PAR'] = 724561
+population['AF-LAG'] = 484952
+population['AF-LOG'] = 426821
+population['AF-SAM'] = 422859
+population['AF-WAR'] = 648866
+population['AF-PKA'] = 762108
+population['AF-PAN'] = 167000
+population['AF-DAY'] = 507610
+
